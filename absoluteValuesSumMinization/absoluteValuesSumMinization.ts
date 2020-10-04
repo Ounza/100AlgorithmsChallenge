f760a67c6bbb6f1bbc5f5d2752ebff193d16ca1c
Rush <rmounza@gmail.com>
2020-10-04 12:40:29 +0300

    Solved first two challenges

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,13 @@
 function absoluteValuesSumMinimization(a: number[]): number {
-
+  let x = a.length;
+  if (x % 2 === 0) {
+    return a[x / 2 - 1];
+  } else {
+    return a[Math.floor(x / 2)];
+  }
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
